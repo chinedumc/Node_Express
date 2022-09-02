f26a9a8e27612d60b4291f23d1242de4046c0bbf
chibokamc <chibokachinedum@gmail.com>
2022-09-02 01:12:10 +0100

    Setting response as plain text or html

--- Node_Init/http.js
+++ Node_Init/http.js
@@ -1,8 +1,14 @@
-const http =require('http')
+const http = require('http')
 
 const server = http.createServer((req, res) => {
+  // console.log('request made')
+  // console.log(req.url, req.method)
   if (req.url === '/'){
+    res.setHeader('Content-Type', 'text/plain')
     res.write('Hello World')
+
+    // res.setHeader('Content-Type', 'text/html')
+    // res.write('<p>Hello world paragraph</p>')
     res.end()
   }
 
@@ -21,6 +27,6 @@ const server = http.createServer((req, res) => {
 
 
 //Raise an Event
-server.listen(3000)
-
-console.log('Listening on port 3000...')
+server.listen(3000, 'localhost', () => {
+  console.log('Listening on port 3000...')
+})  
