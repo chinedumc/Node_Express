c0fe037bfb109a64db9d0f175f3cd787318d5ba8
chibokamc <chibokachinedum@gmail.com>
2022-09-13 23:01:46 +0100

    Used event.EventEmitter  in emitter.js

--- Node_Init/emitter.js
+++ Node_Init/emitter.js
@@ -1,10 +1,18 @@
 const EventEmitter = require('events')
 const emitter = new EventEmitter()
 
+//can be:
+const event = require('events')
+const myEmitter = new event.EventEmitter()
+
+myEmitter.on('readFile', () => {
+  console.log('\nRead Event Occurred!');
+})
+
 //Register a listener
 emitter.on('messageLogged', (arg) => {
   console.log('Listener called', arg)
 })
 
-// Raise an event
+// Raise/Emit an event
 emitter.emit('messageLogged', {id: '1', url: 'http://'})
