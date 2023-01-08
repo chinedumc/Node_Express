4c90354f5ae2136cf15f34f56425091005174f06
chibokamc <chibokachinedum@gmail.com>
2023-01-09 00:26:04 +0100

    Page navigation using sendFile and path

--- Node_Init/app.js
+++ Node_Init/app.js
@@ -1,11 +1,19 @@
-const express = require('express')
+const express = require("express");
+const  path  = require('path');
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('<h1>Home page</h1>')
-})
+const aboutPage = "./htmlFile/about.html"
+// const aboutPage = "./docs2/largeText.txt"
 
+app.get("/", (req, res) => {
+	res.status(200).send("<h1>Home page</h1>");
+});
+
+app.get("/about", (req, res) => {
+  // res.setHeader('Content-Type','text/html')
+	res.sendFile(path.resolve(__dirname, aboutPage))
+});
 
 app.listen(5000,()=>{
 console.log('Server listening on port 5000');
