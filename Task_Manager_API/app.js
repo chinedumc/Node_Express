be7975faf45b3c8daf3308858702facf792e7ab4
chibokamc <chibokachinedum@gmail.com>
2023-01-14 01:18:06 +0100

    Refactored, Added dotenv file

--- Task_Manager_API/app.js
+++ Task_Manager_API/app.js
@@ -1,11 +1,12 @@
-require('./db/connect')
-const express = require('express')
-const app = express()
-const tasks = require('./routes/tasks')
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require('dotenv').config()
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/api/v1/tasks', tasks) 
+app.use("/api/v1/tasks", tasks);
 
 //routes
 // app.get('/api/v1/tasks')
@@ -14,7 +15,17 @@ app.use('/api/v1/tasks', tasks)
 // app.patch('/api/v1/tasks/:id')
 // app.delete('/api/v1/tasks/:id')
 
-const port = 5000
+const port = 5000;
 
-app.listen(port, ()=>{
-  console.log(`Listening on port ${port}`)})
+const start = async () => {
+	try {
+		await connectDB(process.env.MONGO_URI);
+		app.listen(port, () => {
+			console.log(`Listening on port ${port}...`);
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+start();
--- Task_Manager_API/db/connect.js
+++ Task_Manager_API/db/connect.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 
-const connectionString =
-	"mongodb+srv://nedum:nedumMongo@node-and-express.ihhwpd7.mongodb.net/TaskManagerDB?retryWrites=true&w=majority";
+const connectDB = (url) => {
+	return mongoose.connect(url);
+};
 
 mongoose.set("strictQuery", true);
-mongoose
-	.connect(connectionString)
-	.then(() => console.log("CONNECTED TO THE DB..."))
-	.catch((err) => console.log(err));
+
+// .then(() => console.log("CONNECTED TO THE DB..."))
+// .catch((err) => console.log(err));
+
+module.exports = connectDB;
