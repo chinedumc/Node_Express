d6a4a00cab3e01a320c3254c1f15332f6785829f
chibokamc <chibokachinedum@gmail.com>
2023-01-22 00:26:52 +0100

    Added validation to schema

--- Task_Manager_API/controllers/tasks.js
+++ Task_Manager_API/controllers/tasks.js
@@ -1,16 +1,20 @@
-const Task = require('../models/Task')
+const Task = require("../models/Task");
 
 const getAllTasks = (req, res) => {
 	res.send("get all tasks");
 };
 
 const createTask = async (req, res) => {
-	const task = await Task.create(req.body)
-	res.status(201).json({task});
+	try {
+		const task = await Task.create(req.body);
+		res.status(201).json({ task });
+	} catch (error) {
+		res.status(500).json({ msg: error });
+	}
 };
 
 const getTask = (req, res) => {
-	res.json({id:req.params.id});
+	res.json({ id: req.params.id });
 };
 
 const updateTask = (req, res) => {
@@ -24,7 +28,7 @@ const deleteTask = (req, res) => {
 module.exports = {
 	getAllTasks,
 	createTask,
-  getTask,
+	getTask,
 	updateTask,
 	deleteTask,
 };
