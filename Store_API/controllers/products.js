36bae241c0553f4bb4ef76e24bb65f80a9c76858
chibokamc <chibokachinedum@gmail.com>
2023-01-29 23:48:46 +0100

    Refactored to Add queryObject

--- Store_API/controllers/products.js
+++ Store_API/controllers/products.js
@@ -5,7 +5,18 @@ const getAllProductsStatic = async (req, res) => {
 	res.status(200).json({ nbHits: products.length, products });
 };
 const getAllProducts = async (req, res) => {
-	res.status(200).json({ msg: "product route" });
+	const {featured, company} = req.query
+	const queryObject = {}
+
+	if (featured) {
+		queryObject.featured = featured === 'true'?true:false
+	}
+	if(company){
+		queryObject.company = company
+	}
+	console.log(queryObject);
+	const products = await Product.find(queryObject)
+	res.status(200).json({ nbHits:products.length, products });
 };
 
 module.exports = { getAllProductsStatic, getAllProducts };
