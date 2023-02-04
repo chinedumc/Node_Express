419156e968136093621161bd9b5abd803d6716e0
chibokamc <chibokachinedum@gmail.com>
2023-02-04 23:15:50 +0100

    Added custom-error

--- JWT_/controllers/main.js
+++ JWT_/controllers/main.js
@@ -1,4 +1,11 @@
+const CustomAPIError = require("../errors/custom-error");
+
 const login = async (req, res) => {
+	const { username, password } = req.body;
+
+	if (!username || !password) {
+		throw new CustomAPIError("please provide email and password", 400);
+	}
 	res.send("Fake Login/Register/Signup Route");
 };
 
--- JWT_/middleware/error-handler.js
+++ JWT_/middleware/error-handler.js
@@ -1,5 +1,9 @@
+const CustomAPIError = require("../errors/custom-error");
 const errorHandlerMiddlerware = async (err, req, res, next) => {
-	console.log(err);
+	// console.log(err);
+	if (err instanceof CustomAPIError) {
+		return res.status(err.statusCode).json({ msg: err.message });
+	}
 	return res
 		.status(500)
 		.json({ msg: "Something went wrong, please try again" });
