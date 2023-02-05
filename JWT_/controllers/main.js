10b2efe4240d8fc021a68232b53b87c091ab9fe6
chibokamc <chibokachinedum@gmail.com>
2023-02-05 10:47:46 +0100

    Implemented JWT authentication

--- JWT_/controllers/main.js
+++ JWT_/controllers/main.js
@@ -1,3 +1,9 @@
+// check username, password in post(login) request
+// if user exists, create a new JWT
+// send back to FE
+// setup authentication so only the request with JWT can access the dashboard
+
+const jwt = require("jsonwebtoken");
 const CustomAPIError = require("../errors/custom-error");
 
 const login = async (req, res) => {
@@ -6,15 +12,37 @@ const login = async (req, res) => {
 	if (!username || !password) {
 		throw new CustomAPIError("please provide email and password", 400);
 	}
-	res.send("Fake Login/Register/Signup Route");
+	// since we do not have IDs from a DB
+	const id = new Date().getDate();
+
+	const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+		expiresIn: "30d",
+	});
+
+	res.status(200).json({ msg: "user created", token });
 };
 
 const dashboard = async (req, res) => {
-	const luckyNumber = Math.floor(Math.random() * 100);
-	res.status(200).json({
-		msg: `Hello, John Doe`,
-		secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
-	});
+	const authHeader = req.headers.authorization;
+
+	if (!authHeader || !authHeader.startsWith("Bearer ")) {
+		throw new CustomAPIError("Invalid login credentials token", 401);
+	}
+
+	const token = authHeader.split(" ")[1];
+
+	try {
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const luckyNumber = Math.floor(Math.random() * 100);
+    res.status(200).json({
+      msg: `Hello, ${decoded.username}`,
+      secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+    });
+	} catch (error) {
+		throw new CustomAPIError("Not authorized", 401);
+	}
+
 };
 
 module.exports = { login, dashboard };
