4bf8d91d577508fcc35aa81ba88a7c86b8031ba0
chibokamc <chibokachinedum@gmail.com>
2023-02-05 16:00:44 +0100

    Refactored Errors: Bad-request,unauthorized with http-status-codes

--- JWT_/controllers/main.js
+++ JWT_/controllers/main.js
@@ -18,31 +18,18 @@ const login = async (req, res) => {
 	const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
 		expiresIn: "30d",
 	});
-
+	// console.log(token);
 	res.status(200).json({ msg: "user created", token });
 };
 
 const dashboard = async (req, res) => {
-	const authHeader = req.headers.authorization;
+console.log(req.user);
 
-	if (!authHeader || !authHeader.startsWith("Bearer ")) {
-		throw new CustomAPIError("Invalid login credentials token", 401);
-	}
-
-	const token = authHeader.split(" ")[1];
-
-	try {
-		const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    const luckyNumber = Math.floor(Math.random() * 100);
-    res.status(200).json({
-      msg: `Hello, ${decoded.username}`,
-      secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
-    });
-	} catch (error) {
-		throw new CustomAPIError("Not authorized", 401);
-	}
-
+	const luckyNumber = Math.floor(Math.random() * 100);
+	res.status(200).json({
+		msg: `Hello, ${req.user.username}`,
+		secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+	});
 };
 
 module.exports = { login, dashboard };
