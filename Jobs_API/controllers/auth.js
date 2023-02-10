cccd10506a16bf141aa1bfc40c4cd87f4660242d
chibokamc <chibokachinedum@gmail.com>
2023-02-10 12:51:35 +0100

    Generate token - Controller method

--- Jobs_API/controllers/auth.js
+++ Jobs_API/controllers/auth.js
@@ -1,19 +1,23 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError } = require("../errors");
-const bcrypt = require('bcryptjs')
-
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
-  // const { name, email, password } = req.body;
+	// const { name, email, password } = req.body;
 
-  // const salt = await bcrypt.genSalt(10)
-  // const hashedPassword = await bcrypt.hash(password, salt)
+	// const salt = await bcrypt.genSalt(10)
+	// const hashedPassword = await bcrypt.hash(password, salt)
 
-  // const tempUser = {name, email, password:hashedPassword}
+	// const tempUser = {name, email, password:hashedPassword}
 
-  const user = await User.create({...req.body})
-  res.status(StatusCodes.CREATED).json({user})
+	const user = await User.create({ ...req.body });
+	const token = jwt.sign({ userId: user._id, name: user.name }, "jwtSecret", {
+		expiresIn: "30d",
+	});
+	// console.log(user, "useer");
+	// console.log(token, "token");
+	res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
