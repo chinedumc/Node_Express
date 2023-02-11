7b90cf0d8838ca2f545f9c49cde9022d1399f9ec
chibokamc <chibokachinedum@gmail.com>
2023-02-11 20:07:14 +0100

    Added createJob and getAllJobs

---
+++ Jobs_API/models/Jobs.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const JobsSchema = new mongoose.Schema(
+	{
+		company: {
+			type: String,
+			required: [true, "Please provide company name"],
+			maxlength: 50,
+		},
+		position: {
+			type: String,
+			required: [true, "Please provide position"],
+			maxlength: 50,
+		},
+		interview: {
+			type: Boolean,
+			// required:[true,'Please provide interview status']
+			enum: ["interview", "declined", "pending"],
+			default: "pending",
+		},
+		createdBy: {
+			type: mongoose.Types.ObjectId,
+			ref: "User",
+			required: [true, "Please provide a User"],
+		},
+	},
+	{ timestamps: true }
+);
+
+module.exports = mongoose.model("Jobs", JobsSchema);
