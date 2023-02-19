2cb9dcf314f52321f2a5d8bd109b25fb7c8bfafc
chibokamc <chibokachinedum@gmail.com>
2023-02-19 23:24:12 +0100

    Added authController and authRoutes

--- ECOMM_API/app.js
+++ ECOMM_API/app.js
@@ -1,36 +1,37 @@
-require('dotenv').config()
-require('express-async-errors')
+require("dotenv").config();
+require("express-async-errors");
 const express = require("express");
 const app = express();
-const morgan = require('morgan')
+const morgan = require("morgan");
 
-const connectDB = require('./db/connect')
+const connectDB = require("./db/connect");
+
+const authRouter = require("./routes/authRoutes");
 
 //middleware
-const notFoundMiddleware = require('./middleware/not-Found')
-const errorHandlerMiddleware=require('./middleware/error-handler')
+const notFoundMiddleware = require("./middleware/not-Found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.use(morgan("tiny"));
+app.use(express.json());
 
-app.use(morgan('tiny'))
-app.use(express.json())
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
-app.use(notFoundMiddleware)
-app.use(errorHandlerMiddleware)
-
-app.get('/',(req,res)=>{
-  res.send('E-COMM API')
-})
-
-
+//Routes
+app.get("/", (req, res) => {
+	res.send("E-COMM API");
+});
+app.use("/api/v1/auth", authRouter);
 
 const port = process.env.PORT || 5000;
 
 const start = async () => {
 	try {
-    await connectDB(process.env.MONGO_URI)
+		await connectDB(process.env.MONGO_URI);
 		app.listen(port, console.log(`Server listening on port ${port}...`));
 	} catch (error) {
-		console.log(error)
+		console.log(error);
 	}
 };
 
