5393e39a0c8dae2d1fbf51971e13cdea6c59cac2
chibokamc <chibokachinedum@gmail.com>
2023-02-23 23:34:24 +0100

    cookies setup

--- ECOMM_API/app.js
+++ ECOMM_API/app.js
@@ -3,7 +3,9 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cookieParser=require('cookie-parser')
 
+//connect DB
 const connectDB = require("./db/connect");
 
 
@@ -16,10 +18,15 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(morgan("tiny"));
 app.use(express.json());
+app.use(cookieParser())
 
 
 //Routes
 app.get("/", (req, res) => {
+  res.send("E-COMM API");
+});
+app.get("/api/v1", (req, res) => {
+	console.log(req.cookies);
   res.send("E-COMM API");
 });
 app.use("/api/v1/auth", authRouter);
