6c740e220125382d52c35d7d3f5e62cad1b14d71
chibokamc <chibokachinedum@gmail.com>
2023-02-28 19:24:40 +0100

    Added GetAllUsers and GetsingleUser code

--- ECOMM_API/controllers/userController.js
+++ ECOMM_API/controllers/userController.js
@@ -1,10 +1,20 @@
+const User = require('../models/User')
+const { StatusCodes } = require("http-status-codes");
+const CustomError=require('../errors')
+
 // getAllUsers, getSingleUser, ShowCurentUser, updateUser, updateUserPassword
 
+
 const getAllUsers = async (req, res) => {
-	res.send("get all users route");
+  const users = await User.find({role:'user'}).select('-password')
+	res.status(StatusCodes.OK).json({users})
 };
 const getSingleUser = async (req, res) => {
-	res.send("get Single user route");
+	const user = await User.findOne({_id:req.params.id}).select('-password')
+  if(!user){
+    throw new CustomError.NotFoundError(`No user found with id: ${req.params.id}`)
+  }
+  res.status(StatusCodes.OK).json({user})
 };
 const ShowCurentUser = async (req, res) => {
 	res.send("show current user route");
