5d555d21941e9eed0865e4d5cb44f7cfd21ae361
chibokamc <chibokachinedum@gmail.com>
2023-03-02 15:21:49 +0100

    Authenticate user setup

--- ECOMM_API/middleware/authentication.js
+++ ECOMM_API/middleware/authentication.js
@@ -2,14 +2,18 @@ const CustomError = require("../errors");
 const { isTokenValid } = require("../utils");
 
 const authenticateUser = async (req, res, next) => {
-	const token =req.signedCookies.token;
+	const token = req.signedCookies.token;
 
 	if (!token) {
-		console.log("error, no token present");
-	} else {
-		console.log("toekn present");
+		throw new CustomError.UnauthenticatedError("Authentication Invalid");
 	}
-  next()
+	try {
+		const { name, userId, role } = isTokenValid({ token });
+		req.user = { name, userId, role };
+		next();
+	} catch (error) {
+		throw new CustomError.UnauthenticatedError("Authentication Invalid");
+	}
 };
 
 module.exports = { authenticateUser };
