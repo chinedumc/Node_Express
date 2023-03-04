fa39f9be0a547542c53920b5bdeed6d3a541891c
chibokamc <chibokachinedum@gmail.com>
2023-03-05 00:35:21 +0100

    Setup Update Password Cotroller

--- ECOMM_API/controllers/userController.js
+++ ECOMM_API/controllers/userController.js
@@ -24,7 +24,23 @@ const updateUser = async (req, res) => {
 	res.send("update user route");
 };
 const updateUserPassword = async (req, res) => {
-	res.send("update user password");
+	const { oldPassword, newPassword } = req.body;
+	if (!oldPassword || !newPassword) {
+		throw new CustomError.BadRequestError(
+			"Please proveide old and new password"
+		);
+	}
+
+	const user = await User.findOne({ _id: req.user.userId })
+
+  const isPasswordCorrect=await user.comparePasswords(oldPassword)
+  if(!isPasswordCorrect){
+    throw new CustomError.UnauthenticatedError('Invalid Credentials')
+  }
+  user.password=newPassword
+
+  await user.save()
+  res.status(StatusCodes.OK).json({msg:'Sucess! Password updated'})
 };
 
 module.exports = {
