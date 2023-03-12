1734a8e57098cca2f287dd9e6c44f560fc539160
chibokamc <chibokachinedum@gmail.com>
2023-03-12 21:33:28 +0100

    Setup Reviews Model

---
+++ ECOMM_API/models/Review.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const ReviewSchema = mongoose.Schema(
+	{
+		rating: {
+			type: Number,
+			min: 1,
+			max: 5,
+			required: [true, "Please leave a rating"],
+		},
+		title: {
+			type: String,
+			trim: true,
+			required: [true, "Your review needs a title"],
+			maxlength: 100,
+		},
+		comment: {
+			type: String,
+			required: [true, "Enter your review comment"],
+		},
+		user: {
+			type: mongoose.Schema.ObjectId,
+			reference: "User",
+			required: true,
+		},
+		product: {
+			type: mongoose.Schema.ObjectId,
+			reference: "Product",
+			required: true,
+		},
+	},
+	{ timestamps: true }
+);
+
+ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
+
+moduel.exports = mongoose.model("Review", ReviewSchema);
