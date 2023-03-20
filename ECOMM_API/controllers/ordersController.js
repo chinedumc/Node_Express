abab39339e78aad606ecb21ac2716b7e11fee0f7
chibokamc <chibokachinedum@gmail.com>
2023-03-20 10:59:32 +0100

    Setup Order Model, Controller, Routes

---
+++ ECOMM_API/controllers/ordersController.js
@@ -0,0 +1,23 @@
+const createOrder = async (req, res) => {
+	res.send("create orders");
+};
+const getAllOrders = async (req, res) => {
+	res.send("get all orders");
+};
+const getSingleOrder = async (req, res) => {
+	res.send("get single order");
+};
+const getCurrentUserOrders = async (req, res) => {
+	res.send("get current user orders");
+};
+const updateOrder = async (req, res) => {
+	res.send("update orders");
+};
+
+module.exports = {
+	createOrder,
+	getAllOrders,
+	getSingleOrder,
+	getCurrentUserOrders,
+	updateOrder,
+};
